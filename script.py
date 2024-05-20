48b0d539cf03abc0540479b80c28569b8cb7c8a7
Matteo Brandi <matt3o.brandi@gmail.com>
2024-05-20 09:06:37 +0200

    Implementazione dei thread per il controllo dei tasti, da testare

--- script.py
+++ script.py
@@ -7,6 +7,21 @@
 '''
 from pynput.keyboard import Key, Controller
 import RPi.GPIO as GPIO
+from threading import Thread
+
+class pinListener(Thread):
+
+    def __init__(self, pin, key):
+        Thread.__init__(self)
+        self.pin = pin
+        self.key= key
+
+    def run(self):
+        while True:
+            if GPIO.input(pin) == GPIO.HIGH:
+                keyboard.tap(key)
+            if stopFlag == True:
+                return
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
@@ -16,32 +31,40 @@ def setup():
     GPIO.setup(leftP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(rightP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(downP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    print("Setup completato")
+    #print("Setup completato")
 
 def destroy():
     GPIO.cleanup()
+    stopFlag= True
+    downT.join()
 
-def loop():
-    while True:
-        for i in range(len(pins)):
-            if GPIO.input(pins[i]) == GPIO.HIGH:
-                keyboard.tap(keys[i])
+def setThreads():
+    escT = pinListener(pins[0], keys[0])
+    enterT = pinListener(pins[1], keys[1])
+    upT = pinListener(pins[2], keys[2])
+    leftT = pinListener(pins[3], keys[3])
+    rightT = pinListener(pins[4], keys[4])
+    downT = pinListener(pins[5], keys[5])
+
+def startThreads():
+    escT.start()
+    enterT.start()
+    upT.start()
+    leftT.start()
+    rightT.start()
+    downT.start()
 
 pins=[32, 31, 38, 36, 40, 37]
 keys=[Key.esc, Key.enter, Key.up, Key.left, Key.right, Key.down]
 keyboard= Controller()
-
-escP= pins[0]
-enterP= pins[1]
-upP= pins[2]
-leftP= pins[3]
-rightP= pins[4]
-downP= pins[5]
-
+stopFlag= False
 
 setup()
+setThreads()
 
 try:
-    loop()
+    startThreads()
+    while True:
+        pass
 except KeyboardInterrupt:
     destroy()
