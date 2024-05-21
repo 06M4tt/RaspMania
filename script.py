fa76ac58e3c85348d146268f639ad2987b4f7610
Matteo Brandi <matt3o.brandi@gmail.com>
2024-05-21 11:55:54 +0200

    I thread funzionano correttamente, strana eccezione alla loro chiusura

--- script.py
+++ script.py
@@ -14,37 +14,26 @@ class pinListener(Thread):
     def __init__(self, pin, key):
         Thread.__init__(self)
         self.pin = pin
-        self.key= key
+        self.key = key
 
     def run(self):
         while True:
-            if GPIO.input(pin) == GPIO.HIGH:
-                keyboard.tap(key)
+            if GPIO.input(self.pin) == GPIO.HIGH:
+                keyboard.tap(self.key)
+                #print(""+str(self.pin)+"	"+str(self.key))
             if stopFlag == True:
                 return
 
 def setup():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(escP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(enterP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(upP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(leftP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(rightP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    GPIO.setup(downP, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    for i in range(len(pins)):
+        GPIO.setup(pins[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     #print("Setup completato")
 
 def destroy():
     GPIO.cleanup()
     stopFlag= True
     downT.join()
-
-def setThreads():
-    escT = pinListener(pins[0], keys[0])
-    enterT = pinListener(pins[1], keys[1])
-    upT = pinListener(pins[2], keys[2])
-    leftT = pinListener(pins[3], keys[3])
-    rightT = pinListener(pins[4], keys[4])
-    downT = pinListener(pins[5], keys[5])
 
 def startThreads():
     escT.start()
@@ -58,9 +47,14 @@ pins=[32, 31, 38, 36, 40, 37]
 keys=[Key.esc, Key.enter, Key.up, Key.left, Key.right, Key.down]
 keyboard= Controller()
 stopFlag= False
+escT = pinListener(pins[0], keys[0])
+enterT = pinListener(pins[1], keys[1])
+upT = pinListener(pins[2], keys[2])
+leftT = pinListener(pins[3], keys[3])
+rightT = pinListener(pins[4], keys[4])
+downT = pinListener(pins[5], keys[5])
 
 setup()
-setThreads()
 
 try:
     startThreads()
