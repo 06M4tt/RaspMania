14cdd15df4f7787d3bcaa4a604f35e446898b42a
Matteo Brandi <matt3o.brandi@gmail.com>
2024-05-29 08:39:05 +0200

    Aggiunta file README e vari commenti nel codice

---
+++ README.txt
@@ -0,0 +1,12 @@
+Circuit:
+
+                                        1°player									      2°player
+      32(esc)                           38(up)										      12(up)
+                            36(left)                  40(right)						11(left)				15(right)
+      31(enter)                         37(down)									      13(down)
+
+
+This program is thought to be executed in the background meanwhile StepMania is played and a controller is correctly attached to the pins
+It will NOT terminate by itself
+Any error in the circuit will NOT be handled by this program
+The first player will use the arrows, the second the keys w,a,s and d
--- script.py
+++ script.py
@@ -1,15 +1,8 @@
-'''
-                                        1°player												2°player
-      32(esc)                           38(up)													12(up)
-                            36(left)                  40(right)						11(left)				15(right)
-      31(enter)                         37(down)												13(down)
-
-'''
 from pynput.keyboard import Key, Controller, keyCode
 import RPi.GPIO as GPIO
 from threading import Thread
 
-class pinListener(Thread):
+class PinListener(Thread):
 
     def __init__(self, pin, key):
         Thread.__init__(self)
@@ -19,7 +12,7 @@ class pinListener(Thread):
     def run(self):
         while True:
             while GPIO.input(self.pin) == GPIO.HIGH:
-                keyboard.press(self.key)
+                keyboard.press(self.key)        #press the key associated with the pin
                 #print(""+str(self.pin)+"	"+str(self.key))
                 if stopFlag == True:
                     keyboard.release(self.key)
@@ -28,52 +21,59 @@ class pinListener(Thread):
             if stopFlag == True:
                 return
 
+#set the GPIO in BOARD mode and set all the GPIO pins in input mode with pull down resistance
 def setup():
     GPIO.setmode(GPIO.BOARD)
     for i in range(len(pins)):
         GPIO.setup(pins[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    #print("Setup completato")
+    #print("Setup complete")
 
+#safely exit the program after all the threads have stopped
 def destroy():
     GPIO.cleanup()
     stopFlag= True
-    sDownT.join()
+    sDownT.join()       #wait the last thread started to stop
 
+#start all the threads
 def startThreads():
+    #start esc and enter threads
     escT.start()
     enterT.start()
+    #start first player threads
     fUpT.start()
     fLeftT.start()
     fRightT.start()
     fDownT.start()
+    #start second player threads
     sUpT.start()
     sLeftT.start()
     sRightT.start()
     sDownT.start()
+    #print("All the threads have started")
+
 
 pins=[32, 31, 38, 36, 40, 37, 12, 11, 15, 13]
 keys=[Key.esc, Key.enter, Key.up, Key.left, Key.right, Key.down, KeyCode.from_char('w'), KeyCode.from_char('a'), KeyCode.from_char('d'), KeyCode.from_char('s')]
 keyboard= Controller()
 stopFlag= False
 #esc and enter are in common
-escT = pinListener(pins[0], keys[0])
-enterT = pinListener(pins[1], keys[1])
+escT = PinListener(pins[0], keys[0])
+enterT = PinListener(pins[1], keys[1])
 #first player buttons
-fUpT = pinListener(pins[2], keys[2])
-fLeftT = pinListener(pins[3], keys[3])
-fRightT = pinListener(pins[4], keys[4])
-fDownT = pinListener(pins[5], keys[5])
+fUpT = PinListener(pins[2], keys[2])
+fLeftT = PinListener(pins[3], keys[3])
+fRightT = PinListener(pins[4], keys[4])
+fDownT = PinListener(pins[5], keys[5])
 #second player buttons
-sUpT = pinListener(pins[6], keys[6])
-sLeftT = pinListener(pins[7], keys[7])
-sRightT = pinListener(pins[8], keys[8])
-sDownT = pinListener(pins[9], keys[9])
+sUpT = PinListener(pins[6], keys[6])
+sLeftT = PinListener(pins[7], keys[7])
+sRightT = PinListener(pins[8], keys[8])
+sDownT = PinListener(pins[9], keys[9])
 
 setup()
-
 try:
     startThreads()
     while True:
-        pass
+        pass            #the main thread just continously check if CTRL+C is pressed
 except KeyboardInterrupt:
     destroy()
